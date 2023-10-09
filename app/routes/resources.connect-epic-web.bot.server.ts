f319ff447bb1b17ec035ca3a286019de501536d0
Kent C. Dodds <me+github@kentcdodds.com>
2023-10-09 17:25:55 -0600

    reorganize stuff

--- app/routes/resources.connect-epic-web.bot.server.ts
+++ app/routes/resources.connect-epic-web.bot.server.ts
@@ -1,6 +1,7 @@
 import { json, type DataFunctionArgs } from '@remix-run/node'
 import { z } from 'zod'
-import { addEpicWebRoleToUser } from './bot.server'
+import { ref } from '~/bot'
+import { fetchKCDGuild } from '~/bot/utils'
 
 const RequestSchema = z.object({
 	deviceToken: z.string(),
@@ -61,4 +62,31 @@ export async function action({ request }: DataFunctionArgs) {
 	} else {
 		return json(addRoleResult, { status: 500 })
 	}
+}
+
+export async function addEpicWebRoleToUser(userId: string) {
+	const guild = await getGuild()
+	if (!guild) return { status: 'error', error: 'KCD Guild not found' } as const
+	const epicWebRoleId = process.env.ROLE_ID_EPIC_WEB
+	await guild.members.fetch(userId)
+	const member = guild.members.cache.get(userId)
+	if (!member) return { status: 'error', error: 'Member not found' } as const
+	await member.roles.add(epicWebRoleId)
+	await member.setNickname(`${member.displayName} 🌌`)
+	return { status: 'success', member } as const
+}
+
+async function getGuild() {
+	const { client } = ref
+	if (!client) {
+		console.error('no client', ref)
+		return
+	}
+
+	const guild = await fetchKCDGuild(client)
+	if (!guild) {
+		console.error('KCD Guild not found')
+		return
+	}
+	return guild
 }
