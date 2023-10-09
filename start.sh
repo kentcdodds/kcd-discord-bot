630fb8249565bdf1c77fb1df8d8f4753859e43bc
Kent C. Dodds <me+github@kentcdodds.com>
2023-10-09 17:13:27 -0600

    remove prisma for realz

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
-npx prisma migrate deploy
 # node ./scripts/deploy-commands.js
 # node ./scripts/deploy-emoji.js
 npm run start
