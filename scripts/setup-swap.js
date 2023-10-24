637a0a26a06698da49a6e1012be6163e6bdb4e52
Kent C. Dodds <me+github@kentcdodds.com>
2023-10-23 19:29:30 -0600

    whoops, forgot esm

--- scripts/setup-swap.js
+++ scripts/setup-swap.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
-import { writeFile } from 'node:fs/promises'
-import { $ } from 'execa'
+const { writeFile } = require('fs').promises
+const { $ } = require('execa')
 
-console.log('setting up swapfile...')
-await $`fallocate -l 256M /swapfile`
-await $`chmod 0600 /swapfile`
-await $`mkswap /swapfile`
-await writeFile('/proc/sys/vm/swappiness', '10')
-await $`swapon /swapfile`
-await writeFile('/proc/sys/vm/overcommit_memory', '1')
-console.log('swapfile setup complete')
+async function go() {
+	console.log('setting up swapfile...')
+	await $`fallocate -l 512M /swapfile`
+	await $`chmod 0600 /swapfile`
+	await $`mkswap /swapfile`
+	await writeFile('/proc/sys/vm/swappiness', '10')
+	await $`swapon /swapfile`
+	await writeFile('/proc/sys/vm/overcommit_memory', '1')
+	console.log('swapfile setup complete')
+}
+
+go()
